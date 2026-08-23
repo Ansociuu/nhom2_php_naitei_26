02d9f23f085534a8c25bc216b615f257bea9c7bb
Ansociuu <nk.anbmtabc@gmail.com>
2026-08-23 16:43:59 +0000

    feat: add auth API

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TourController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+    // Auth API
+    Route::prefix('auth')->group(function () {
+        // Public Auth Routes
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+
+        // Protected Auth Routes (Require Bearer Token)
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/me', [AuthController::class, 'me']);
+            Route::put('/profile', [AuthController::class, 'updateProfile']);
+            Route::put('/password', [AuthController::class, 'updatePassword']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
+    });
+
     // Categories API
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{category}', [CategoryController::class, 'show']);
